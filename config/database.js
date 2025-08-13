49089de9992734c45c6c713555a6aebc23231a3a
vivtv <vtv.villalobos@gmail.com>
2025-08-13 08:16:05 +0700

    attendance update

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || 'marshmelloX_X55',
     database: process.env.DB_NAME || 'university_attendance',
     waitForConnections: true,
     connectionLimit: 10,
